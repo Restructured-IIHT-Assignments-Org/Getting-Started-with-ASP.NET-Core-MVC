1e893abeeb9f1874f1724f2180c0fe23eead0881
shobhitsharma123 <63493884+shobhitsharma123@users.noreply.github.com>
2020-04-14 19:13:09 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Getting_Started_with_ASP.NET_Core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Quick Start – Build and Run Your First Application/helloworld.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Getting_Started_with_ASP.NET_Core_MVC.Quick_Start___Build_and_Run_Your_First_Application
+{
+    public class Class
+    {
+    }
+}
